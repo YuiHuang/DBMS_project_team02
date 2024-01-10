63b06e8a02802eefa1be7b97cba31725364c9900
Amber-coding <amber.555yi@gmail.com>
2024-01-10 23:13:33 +0800

    0110  commit

--- like_button.js
+++ like_button.js
@@ -9,14 +9,28 @@ class LikeButton extends React.Component {
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
+    const buttonStyle = {
+      fontSize: '16px',
+      padding: '10px 20px',
+      color: this.state.liked ? '#fff' : '#000',
+      backgroundColor: this.state.liked ? '#28a745' : '#007bff',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+      outline: 'none',
+      transition: 'background-color 0.3s'
+    };
+
+    const buttonText = this.state.liked ? 'You liked this' : 'Like';
 
     return e(
       'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+      {
+        onClick: () => this.setState({ liked: !this.state.liked }),
+        style: buttonStyle
+      },
+      this.state.liked ? e('i', { className: 'fas fa-thumbs-up' }) : '',
+      buttonText
     );
   }
 }
@@ -24,3 +38,31 @@ class LikeButton extends React.Component {
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
 root.render(e(LikeButton));
+
+
+// 'use strict';
+
+// const e = React.createElement;
+
+// class LikeButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { liked: false };
+//   }
+
+//   render() {
+//     if (this.state.liked) {
+//       return 'You liked this.';
+//     }
+
+//     return e(
+//       'button',
+//       { onClick: () => this.setState({ liked: true }) },
+//       'Like'
+//     );
+//   }
+// }
+
+// const domContainer = document.querySelector('#like_button_container');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(e(LikeButton));
